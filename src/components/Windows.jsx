d08ae5560d2639a3063ba7fc495e711914cc79cd
bokii25 <bokisa1515@gmail.com>
2026-09-21 12:18:40 +0200

    Windows Object adde

--- src/components/Windows.jsx
+++ src/components/Windows.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { Navbar } from "./Navbar";
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import '../folder.css'
 
 export const Windows = () => {
